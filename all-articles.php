620fd956a4d13a9ccf24ec85a13970cd23e2c75b
Patryk Rutkowski <rutkowskipatryk1@gmail.com>
2020-06-16 13:14:32 +0200

    Include header-login.php

--- all-articles.php
+++ all-articles.php
@@ -1,33 +1,10 @@
 <?php
-include_once 'templates/header.php';
+include_once('templates/header-login.php');
 
 if(!isset($_SESSION['login'])) {
     header('Location: login.php');
 }
 ?>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="all-articles.php">Wszystkie artykuły</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add-article.php">Dodaj artykuł</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="my-articles.php">Twoje artykuły</a>
-                </li>
-              </ul>
-          <a href="logout.php"><button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Wyloguj</button></a>
-        </div>
-      </nav>
-
 <div class="container">
     <div class="jumbotron">
         <div class="row">
